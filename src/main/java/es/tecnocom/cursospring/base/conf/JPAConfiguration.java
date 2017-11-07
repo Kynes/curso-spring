9939cd8304abdc3ceb4e33df4ea3bf1f33596253
Eduardo Ramírez <eduardo.ramirez.ronco@gmail.com>
2017-11-07 14:14:56 +0100

    Change database to h2.

--- src/main/java/es/tecnocom/cursospring/base/conf/JPAConfiguration.java
+++ src/main/java/es/tecnocom/cursospring/base/conf/JPAConfiguration.java
@@ -1,59 +1,50 @@
 package es.tecnocom.cursospring.base.conf;
 
-import java.util.Properties;
-
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableTransactionManagement
 @ComponentScan( basePackages = { "es.tecnocom.cursospring.*" } )
 public class JPAConfiguration {
-  @Bean
-  public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-    LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
-    em.setDataSource( dataSource() );
-    em.setPackagesToScan( new String[] { "es.tecnocom.cursospring.*" } );
-    JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-    em.setJpaVendorAdapter( vendorAdapter );
-    em.setJpaProperties( additionalProperties() );
-    return em;
-  }
 
   @Bean
   public DataSource dataSource() {
-    DriverManagerDataSource dataSource = new DriverManagerDataSource();
-    dataSource.setDriverClassName( "org.postgresql.Driver" );
-    dataSource.setUrl( "jdbc:postgresql://127.0.0.1:5432/jsf_spring_jpa_junit_integration" );
-    dataSource.setUsername( "teste" );
-    dataSource.setPassword( "teste" );
-    return dataSource;
-  }
-
-  private Properties additionalProperties() {
-    Properties properties = new Properties();
-    properties.setProperty( "hibernate.hbm2ddl.auto", "create-drop" );
-    properties.setProperty( "hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect" );
-    properties.setProperty( "hibernate.show_sql", "true" );
-    properties.setProperty( "hibernate.format_sql", "true" );
-    return properties;
+    return new EmbeddedDatabaseBuilder().setType( EmbeddedDatabaseType.H2 ).build();
   }
 
   @Bean
-  public PlatformTransactionManager transactionManager( EntityManagerFactory emf ) {
-    JpaTransactionManager transactionManager = new JpaTransactionManager();
-    transactionManager.setEntityManagerFactory( emf );
-    return transactionManager;
+  public JpaVendorAdapter jpaVendorAdapter() {
+    HibernateJpaVendorAdapter bean = new HibernateJpaVendorAdapter();
+    bean.setDatabase( Database.H2 );
+    bean.setGenerateDdl( true );
+    return bean;
+  }
+
+  @Bean
+  public LocalContainerEntityManagerFactoryBean entityManagerFactory( DataSource dataSource,
+      JpaVendorAdapter jpaVendorAdapter ) {
+    LocalContainerEntityManagerFactoryBean bean = new LocalContainerEntityManagerFactoryBean();
+    bean.setDataSource( dataSource );
+    bean.setJpaVendorAdapter( jpaVendorAdapter );
+    bean.setPackagesToScan( "es.tecnocom.cursospring" );
+    return bean;
+  }
+
+  @Bean
+  public JpaTransactionManager transactionManager( EntityManagerFactory emf ) {
+    return new JpaTransactionManager( emf );
   }
 }
